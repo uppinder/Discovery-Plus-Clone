345df030d10a8753b8eebde75cb7936a1aeb50eb
Uppinder Chugh <uppinderchugh@gmail.com>
2023-10-18 09:29:31 +0530

    Scroll to top on location change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react';
 import Router from './Routes';
-
-const theme = extendTheme({
-  styles: {
-    global: {
-      // styles for the `body`
-      body: {
-        color: 'rgba(255, 255, 255, 0.92)',
-      },
-    },
-  },
-});
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  const theme = extendTheme({
+    styles: {
+      global: {
+        // styles for the `body`
+        body: {
+          color: 'rgba(255, 255, 255, 0.92)',
+        },
+      },
+    },
+  });
+
   return (
     <ChakraProvider theme={theme}>
       <Box backgroundColor="#1A1C21" minH="100vh">
